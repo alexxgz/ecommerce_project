ba4c241efb2625253de6ab69f4e886b05177b098
alexgz <alexinc19@gmail.com>
2021-02-04 23:05:45 -0800

    I got django up and running as well as establised basic routes and views

---
+++ ecomm_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('women/', views.women, name='women'),
+    path('men/', views.men, name='men'),
+    path('accessories/', views.accessories, name='accessories'),
+    path('orders', views.orders, name='orders'),    
+]
---
+++ ecomm_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def women(request):
+    return render(request, 'women.html')
+
+def men(request):
+    return render(request, 'men.html')
+
+def accessories(request):
+    return render(request, 'accessories.html')
+
+def orders(request):
+    return render(request, 'orders.html')
