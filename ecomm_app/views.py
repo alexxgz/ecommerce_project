fb574ea050604a0c7269f396289d61b8f69dd5e8
alexgz <alexinc19@gmail.com>
2021-02-07 02:25:48 -0800

    Got log in working properly but without the modal

--- ecomm_app/views.py
+++ ecomm_app/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -24,3 +23,17 @@ def orders(request):
 
 def account(request):
     return render(request, 'users/account.html')
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('account')    
+    context = {}
+    return render(request, 'registration/login.html', context)
