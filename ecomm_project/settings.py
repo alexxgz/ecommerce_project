85fb8c6955d4248d4fa7fe47a8e5a3960a0b2516
alexgz <alexinc19@gmail.com>
2021-02-13 13:24:54 -0800

    Added key to heroku dashboard

--- ecomm_project/settings.py
+++ ecomm_project/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_on_heroku
-from boto.s3.connection import S3Connection
-s3 = S3Connection(os.environ['KEY'], os.environ['SECRET_KEY'])
+# from boto.s3.connection import S3Connection
+# s3 = S3Connection(os.environ['KEY'], os.environ['SECRET_KEY'])
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
