7831e4a5d1bbc6ea2d06ac19c7774e4d95b3f725
alexgz <alexinc19@gmail.com>
2021-02-13 10:54:29 -0800

    Added boto s3

--- ecomm_project/settings.py
+++ ecomm_project/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from .secret_settings import *
 import os
 import django_on_heroku
-
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['SECRET_KEY'])
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
