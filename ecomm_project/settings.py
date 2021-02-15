c4462422aa069f5568a720fb61e1bb9e18d4a907
alexgz <alexinc19@gmail.com>
2021-02-15 00:50:19 -0800

    Added env file for secret key

--- ecomm_project/settings.py
+++ ecomm_project/settings.py
@@ -16,6 +16,13 @@ import django_on_heroku
 # from boto.s3.connection import S3Connection
 # s3 = S3Connection(os.environ['KEY'], os.environ['SECRET_KEY'])
 
+import environ 
+env = environ.Env()
+environ.Env.read_env()
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
